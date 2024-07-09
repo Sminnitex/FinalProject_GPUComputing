4bba950899ae699cd4d7d3a1f32d55536a1233f2
Sminnitex <mchlminniti@gmail.com>
2024-07-09 19:11:18 +0200

    plot

--- plot.py
+++ plot.py
@@ -2,61 +2,77 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df0 = pd.read_csv("output/Matrix0.csv")
-df1 = pd.read_csv("output/Matrix1.csv")
-df2 = pd.read_csv("output/Matrix2.csv")
-df3 = pd.read_csv("output/Matrix3.csv")
-df4 = pd.read_csv("output/Matrix4.csv")
-df5 = pd.read_csv("output/Matrix5.csv")
-df6 = pd.read_csv("output/Matrix6.csv")
-df7 = pd.read_csv("output/Matrix7.csv")
-df8 = pd.read_csv("output/Matrix8.csv")
-#df9 = pd.read_csv("output/Matrix9.csv")
+df0 = pd.read_csv("output/Matrix0.csv", skiprows=1)
+df1 = pd.read_csv("output/Matrix1.csv", skiprows=1)
+df2 = pd.read_csv("output/Matrix2.csv", skiprows=1)
+df3 = pd.read_csv("output/Matrix3.csv", skiprows=1)
+df4 = pd.read_csv("output/Matrix4.csv", skiprows=1)
+df5 = pd.read_csv("output/Matrix5.csv", skiprows=1)
+df6 = pd.read_csv("output/Matrix6.csv", skiprows=1)
+df7 = pd.read_csv("output/Matrix7.csv", skiprows=1)
+df8 = pd.read_csv("output/Matrix8.csv", skiprows=1)
+df9 = pd.read_csv("output/Matrix9.csv", skiprows=1)
 
-fig, axes = plt.subplots(2, 1, figsize=(10, 8))
+matrixNNZ = np.array([df0["NonZeros"][0], df1["NonZeros"][0], df2["NonZeros"][0], df3["NonZeros"][0], df4["NonZeros"][0], df5["NonZeros"][0], df6["NonZeros"][0], df7["NonZeros"][0], df8["NonZeros"][0], df9["NonZeros"][0]])
+cusparseBand = np.array([df0['Bandwidth'][0], df1['Bandwidth'][0], df2['Bandwidth'][0], df3['Bandwidth'][0], df4['Bandwidth'][0], df5['Bandwidth'][0], df6['Bandwidth'][0], df7['Bandwidth'][0], df8['Bandwidth'][0], df9['Bandwidth'][0]])
+globalBand = np.array([df0['Bandwidth'][1], df1['Bandwidth'][1], df2['Bandwidth'][1], df3['Bandwidth'][1], df4['Bandwidth'][1], df5['Bandwidth'][1], df6['Bandwidth'][1], df7['Bandwidth'][1], df8['Bandwidth'][1], df9['Bandwidth'][1]])
+sharedBand = np.array([df0['Bandwidth'][2], df1['Bandwidth'][2], df2['Bandwidth'][2], df3['Bandwidth'][2], df4['Bandwidth'][2], df5['Bandwidth'][2], df6['Bandwidth'][2], df7['Bandwidth'][2], df8['Bandwidth'][2], df9['Bandwidth'][2]])
+mySparseBand = np.array([df0['Bandwidth'][3], df1['Bandwidth'][3], df2['Bandwidth'][3], df3['Bandwidth'][3], df4['Bandwidth'][3], df5['Bandwidth'][3], df6['Bandwidth'][3], df7['Bandwidth'][3], df8['Bandwidth'][3], df9['Bandwidth'][3]])
+matrixDIM = np.array([df0["Rows"][0] * df0["Columns"][0], df1["Rows"][0] * df1["Columns"][0], df2["Rows"][0] * df2["Columns"][0], df3["Rows"][0] * df3["Columns"][0], df4["Rows"][0] * df4["Columns"][0], df5["Rows"][0] * df5["Columns"][0], df6["Rows"][0] * df6["Columns"][0], df7["Rows"][0] * df7["Columns"][0], df8["Rows"][0] * df8["Columns"][0], df9["Rows"][0] * df9["Columns"][0]])
 
-axes[0].plot(df['Dimensions'], df['Bandwidth'], label='blk-grid size 64-14', color='blue')
-axes[0].plot(df2['Dimensions'], df2['Bandwidth'], label='32-7', color='red')
-axes[0].plot(df3['Dimensions'], df3['Bandwidth'], label='16-3', color='green')
-axes[0].plot(df4['Dimensions'], df4['Bandwidth'], label='8-1', color='yellow')
-axes[0].set_title('Shared Memory Matrix Transpose')
-axes[0].legend()
+#Get the sorted indices
+sorted_indices_nnz = np.argsort(matrixNNZ)
+sorted_indices_dim = np.argsort(matrixDIM)
 
-# Plot second graph
-axes[1].plot(df5['Dimensions'], df5['Time'], label='64-14', color='blue')
-axes[1].plot(df6['Dimensions'], df6['Time'], label='32-7', color='red')
-axes[1].plot(df7['Dimensions'], df7['Time'], label='16-3', color='green')
-axes[1].plot(df8['Dimensions'], df8['Time'], label='8-1', color='yellow')
-axes[1].set_title('Global Memory Matrix Transpose')
-axes[1].legend()
+#Plot by nonzeros
+sorted_matrixNNZ = matrixNNZ[sorted_indices_nnz]
+sorted_cusparseBand = cusparseBand[sorted_indices_nnz]
+sorted_globalBand = globalBand[sorted_indices_nnz]
+sorted_sharedBand = sharedBand[sorted_indices_nnz]
+sorted_mySparseBand = mySparseBand[sorted_indices_nnz]
 
-plt.xlabel("Matrix n*n dimension")
-plt.ylabel("Time (s)")
-plt.tight_layout()
-plt.xlim(1024, 2047)
-plt.ylim(0, 0.0140)
-plt.show()
+fig1, axes1 = plt.subplots(figsize=(10, 8))
 
-fig, axes = plt.subplots(2, 1, figsize=(10, 8))
-bandwidth1 = (34358647168 / np.power(10, 9)) / 1.3184719999999999
-bandwidth2 = (34358647168 / np.power(10, 9)) / 0.027733
-bandwidth3 = (34358647168 / np.power(10, 9)) / 0.016306
-bandwidth4 = (34358647168 / np.power(10, 9)) / 0.013561
-bandwidth5 = (34358647168 / np.power(10, 9)) / 0.669816
-bandwidth6 = (34358647168 / np.power(10, 9)) / 0.067616
-bandwidth7 = (34358647168 / np.power(10, 9)) / 0.017985
-bandwidth8 = (34358647168 / np.power(10, 9)) / 0.013659999999999999
-firstplot = [bandwidth1, bandwidth2, bandwidth3, bandwidth4]
-secondplot = [bandwidth5, bandwidth6, bandwidth7, bandwidth8]
+axes1.plot(sorted_matrixNNZ, sorted_cusparseBand, label='Cusparse', color='blue')
+axes1.plot(sorted_matrixNNZ, sorted_globalBand, label='Global', color='red')
+axes1.plot(sorted_matrixNNZ, sorted_sharedBand, label='Shared', color='green')
+axes1.plot(sorted_matrixNNZ, sorted_mySparseBand, label='MySparse', color='yellow')
 
-axes[0].plot(["64-14", "32-7", "16-3", "8-1"], firstplot, color='blue')
-axes[0].set_title('Shared Memory Matrix Transpose')
+axes1.set_xlabel('NonZeros')
+axes1.set_ylabel('Bandwidth')
+axes1.legend()
 
-# Plot second graph
-axes[1].plot(["64-14", "32-7", "16-3", "8-1"], secondplot, color='blue')
-axes[1].set_title('Global Memory Matrix Transpose')
+#Plot by matrix dim
+fig2, axes2 = plt.subplots(figsize=(10, 8))
+sorted_matrixDIM = matrixDIM[sorted_indices_dim]
+sorted_cusparseBand = cusparseBand[sorted_indices_dim]
+sorted_globalBand = globalBand[sorted_indices_dim]
+sorted_sharedBand = sharedBand[sorted_indices_dim]
+sorted_mySparseBand = mySparseBand[sorted_indices_dim]
 
-plt.xlabel("Block and grid sizes")
-plt.ylabel("Bandwidth GB/s")
+axes2.plot(sorted_matrixDIM, sorted_cusparseBand, label='Cusparse', color='blue')
+axes2.plot(sorted_matrixDIM, sorted_globalBand, label='Global', color='red')
+axes2.plot(sorted_matrixDIM, sorted_sharedBand, label='Shared', color='green')
+axes2.plot(sorted_matrixDIM, sorted_mySparseBand, label='MySparse', color='yellow')
+
+axes2.set_xlabel('Dimensions')
+axes2.set_ylabel('Bandwidth')
+axes2.legend()
+
+# Plot by files
+fig3, axes3 = plt.subplots(figsize=(10, 7))
+file_indices = np.arange(len(matrixNNZ))
+
+axes3.plot(file_indices, cusparseBand, label='Cusparse', color='blue')
+axes3.plot(file_indices, globalBand, label='Global', color='red')
+axes3.plot(file_indices, sharedBand, label='Shared', color='green')
+axes3.plot(file_indices, mySparseBand, label='MySparse', color='yellow')
+
+axes3.set_xticks(file_indices)
+axes3.set_xticklabels(['1138_bus', 'Maragal_3', 'photogrammetry', 'plbuckle', 'bcsstk17', 'filter2D', 'SiH4', 'linverse', 't2dah_a', 'barrier2-10'], rotation=45)
+axes3.set_xlabel('Files')
+axes3.set_ylabel('Bandwidth')
+axes3.legend()
+
 plt.tight_layout()
 plt.show()
